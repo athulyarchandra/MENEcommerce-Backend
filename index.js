731e7f39e4df9b333b9dd308bddd5148f1fdd9a7
athulyarchandra <athulyarchandra24@gmail.com>
2025-11-12 00:16:33 +0530

    cors edit

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server started running at http://localhost:${PORT}`);
 });
